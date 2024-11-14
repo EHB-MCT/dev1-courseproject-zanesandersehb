0a3a2ae772d405f403fa591b7a0aa3be2b3f943c
Zane Sanders <zane.sanders@student.ehb.be>
2024-11-14 10:12:40 +0100

    driehoeken

--- generativedesign.js
+++ generativedesign.js
@@ -17,22 +17,48 @@ function drawRandomShapes() {
   let numCircles = Utils.randomNumber(3, 7);
   let numRectangles = Utils.randomNumber(2, 4);
   let numTriangles = Utils.randomNumber(2, 5);
-}
-// functie voor cirkels
-for (let i = 0; i < numCircles; i++) {
-  let radius = Utils.randomNumber(0.05 * width, 0.1 * width);
-  let x = Utils.randomNumber(0, width);
-  let y = Utils.randomNumber(0, height);
-  context.fillStyle = Utils.randomColor();
-  Utils.fillCircle(x, y, radius);
+
+  // functie voor cirkels
+  for (let i = 0; i < numCircles; i++) {
+    let radius = Utils.randomNumber(0.05 * width, 0.1 * width);
+    let x = Utils.randomNumber(0, width);
+    let y = Utils.randomNumber(0, height);
+    context.fillStyle = Utils.randomColor();
+    Utils.fillCircle(x, y, radius);
+  }
+
+  // functie voor rechthoeken
+  for (let i = 0; i < numRectangles; i++) {
+    let rectWidth = Utils.randomNumber(0.1 * width, 0.2 * width);
+    let rectHeight = Utils.randomNumber(0.1 * height, 0.2 * height);
+    let x = Utils.randomNumber(0, width - rectWidth);
+    let y = Utils.randomNumber(0, height - rectHeight);
+    context.fillStyle = Utils.randomColor();
+    context.fillRect(x, y, rectWidth, rectHeight);
+  }
+
+  // functie voor driehoeken
+  for (let i = 0; i < numTriangles; i++) {
+    context.beginPath();
+    let x1 = Utils.randomNumber(0, width);
+    let y1 = Utils.randomNumber(0, height);
+    let x2 = Utils.randomNumber(0, width);
+    let y2 = Utils.randomNumber(0, height);
+    let x3 = Utils.randomNumber(0, width);
+    let y3 = Utils.randomNumber(0, height);
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.lineTo(x3, y3);
+    context.closePath();
+    context.fillStyle = Utils.randomColor();
+    context.fill();
+  }
 }
 
-// functie voor rechthoeken
-for (let i = 0; i < numRectangles; i++) {
-  let rectWidth = Utils.randomNumber(0.1 * width, 0.2 * width);
-  let rectHeight = Utils.randomNumber(0.1 * height, 0.2 * height);
-  let x = Utils.randomNumber(0, width - rectWidth);
-  let y = Utils.randomNumber(0, height - rectHeight);
-  context.fillStyle = Utils.randomColor();
-  context.fillRect(x, y, rectWidth, rectHeight);
+function init() {
+  resizeCanvas();
+  drawRandomShapes();
 }
+
+window.addEventListener("resize", init);
+window.addEventListener("load", init);
